201cd25a3da743a34d1375cd674dced8272d0079
strangejmaster <strangejmaster@gmail.com>
2026-03-29 16:54:12 -0500

    Added number sanitization to lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,14 +37,28 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def sanitize_input(target_type, prompt):
+    """Returns a sanitized value of the type of the requested class"""
+
+    while (True):
+        value = input(prompt)
+        try:    
+            value = target_type(value)
+        except:
+            pass
+        if (isinstance(value, target_type)):
+            return value
+        else:
+            print(f"That was not a value of type {target_type}; please try again")
+        
 def main():
     
     print(f"===== Simple Calculator =====")
-
+    
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = sanitize_input(float, "Enter the first number: ")
+    num2 = sanitize_input(float, "Enter the second number: ")
+    operation = sanitize_input(str, "Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
